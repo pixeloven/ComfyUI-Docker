c0b87a28acdd01e313219fb8277f75cb91414422
Brian Gebel <briangeb@gmail.com>
2025-03-19 18:03:14 -0700

    Rename to main

--- services/download/download.sh
+++ services/download/download.sh
@@ -23,9 +23,9 @@ parallel --will-cite -a /docker/checksums.sha256 "echo -n {} | sha256sum -c"
 
 cat <<EOF
 By using this software, you agree to the following licenses:
-https://github.com/AbdBarho/stable-diffusion-webui-docker/blob/master/LICENSE
+https://github.com/AbdBarho/stable-diffusion-webui-docker/blob/main/LICENSE
 https://github.com/CompVis/stable-diffusion/blob/main/LICENSE
-https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/LICENSE.txt
+https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/main/LICENSE.txt
 https://github.com/invoke-ai/InvokeAI/blob/main/LICENSE
 And licenses of all UIs, third party libraries, and extensions.
 EOF
