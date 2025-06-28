4ac7b451f41ce883494b7b7c0f0acc51323a0c21
Brian Gebel <bgebel@gofundme.com>
2025-06-27 19:11:52 -0700

    use manifest instead of text

--- services/comfy-setup/entrypoint.sh
+++ services/comfy-setup/entrypoint.sh
@@ -14,16 +14,28 @@ mkdir -vp /data/embeddings \
  if [[ "$SETUP_DRY_RUN" -eq 1 ]]; then
   echo "Running Dry Run..."
 
-  aria2c --dry-run -x 10 --disable-ipv6 --input-file ./links.txt --dir /data/models --continue
+  aria2c --dry-run -x 10 --disable-ipv6 --input-file  ./model.manifest --dir /data/models --continue
 
 else
   echo "Downloading, this might take a while..."
 
-  aria2c -x 10 --disable-ipv6 --input-file ./links.txt --dir /data/models --continue
+  aria2c -x 10 --disable-ipv6 --input-file ./model.manifest --dir /data/models --continue
 
   echo "Checking SHAs..."
 
   parallel --will-cite -a ./checksums.sha256 "echo -n {} | sha256sum -c"
 fi
+
+# https://github.com/Comfy-Org/comfy-cli
+# Can we use CLI to setup comfy.
+# Can the CLI install extnesions or does the manager have a way to do that via CLI?
+# Can we use the maninfest format for the CLI?
+# -extensions 
+# --ipadapter
+# ---requirements.txt
+# ---model.manifest
+# --comfyui-manager
+# Or should we do it by "packs"? slim, mega, etc
+
 
 exec "$@"
