5eb7e4570441fdf9d083f8a9f309852c3fdbf41f
Brian Gebel <Brian Gebel>
2025-07-06 15:50:25 -0700

    update runner

--- services/comfy/runner.sh
+++ services/comfy/runner.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 set -e
 
 # Activate virtual environment
 # source venv/bin/activate
 
 # Start ComfyUI with the specified parameters
-comfy launch -- --listen --port $COMFY_PORT --base-directory $BASE_DIRECTORY --output-directory $OUTPUT_DIRECTORY $CLI_ARGS
+comfy launch -- --listen --port $COMFY_PORT --base-directory $BASE_DIRECTORY --output-directory $OUTPUT_DIRECTORY $CLI_ARG
