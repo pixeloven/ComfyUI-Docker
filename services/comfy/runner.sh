dc72f28e24d2730f26c1b4946123cf9c1dfcd577
Brian Gebel <Brian Gebel>
2025-07-06 15:49:05 -0700

    update dockerfile

--- services/comfy/runner.sh
+++ services/comfy/runner.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-python -u main.py --listen --port $COMFY_PORT --base-directory $BASE_DIRECTORY --output-directory $OUTPUT_DIRECTORY $CLI_ARGS
+# Activate virtual environment
+# source venv/bin/activate
+
+# Start ComfyUI with the specified parameters
+comfy launch -- --listen --port $COMFY_PORT --base-directory $BASE_DIRECTORY --output-directory $OUTPUT_DIRECTORY $CLI_ARGS
