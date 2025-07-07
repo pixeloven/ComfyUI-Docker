76ab93f87dc51d1bd0f8ed5a405bc4ddc093278e
Brian Gebel <Brian Gebel>
2025-07-06 20:52:16 -0700

    save setup

--- services/comfy/runner.sh
+++ services/comfy/runner.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Activate virtual environment
-# source venv/bin/activate
+source ~/.venv/bin/activate
 
 # Start ComfyUI with the specified parameters
 comfy launch -- --listen --port $COMFY_PORT --base-directory $BASE_DIRECTORY --output-directory $OUTPUT_DIRECTORY $CLI_ARG
