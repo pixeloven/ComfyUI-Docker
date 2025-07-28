d8377af344a46b7208b82cfe9b2cf177e0841177
Brian Gebel <Brian Gebel>
2025-07-28 09:53:34 -0700

    comment out nodes for now

---
+++ services/comfy/extended/scripts/11-install-pulid-flux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# # PuLID_ComfyUI_FLUX (Enhanced implementation with SDXL support)
+# # @description: Enhanced PuLID implementation with additional features and SDXL support.
+# # @link: https://github.com/lschaupp/PuLID_ComfyUI_FLUX
+# if [ ! -d "$COMFY_BASE_DIRECTORY/custom_nodes/PuLID_ComfyUI_FLUX" ]; then
+#     echo "Installing PuLID_ComfyUI_FLUX..."
+#     git clone --branch main --depth 1 https://github.com/lschaupp/PuLID_ComfyUI_FLUX.git ${COMFY_BASE_DIRECTORY}/custom_nodes/PuLID_ComfyUI_FLUX
+#     pip install -r ${COMFY_BASE_DIRECTORY}/custom_nodes/PuLID_ComfyUI_FLUX/requirements.txt
+# else
+#     echo "PuLID_ComfyUI_FLUX already installed"
+# fi
+
+# https://github.com/lldacing/ComfyUI_PuLID_Flux_ll - Does it support SDXL?
---
+++ services/comfy/extended/scripts/12-install-tea-cache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# ComfyUI-TeaCache
+# @description: TeaCache is a cache for ComfyUI that is not part of the official ComfyUI repository.
+# @link: https://github.com/welltop-cn/ComfyUI-TeaCache.git
+# if [ ! -d "$COMFY_BASE_DIRECTORY/custom_nodes/ComfyUI-TeaCache" ]; then
+#     echo "Installing ComfyUI-TeaCache..."
+#     git clone --branch main --depth 1 https://github.com/welltop-cn/ComfyUI-TeaCache.git ${COMFY_BASE_DIRECTORY}/custom_nodes/ComfyUI-TeaCache
+# else
+#     echo "ComfyUI-TeaCache already installed"
+# fi
+
+# Note worthy https://github.com/chengzeyi/Comfy-WaveSpeed
