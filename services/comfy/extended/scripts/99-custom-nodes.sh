7c6e261b42f7db84d21bfb3e3e8b4b3dd44f6593
Brian Gebel <Brian Gebel>
2025-07-28 17:48:13 -0700

    got it

--- services/comfy/extended/scripts/99-custom-nodes.sh
+++ services/comfy/extended/scripts/99-custom-nodes.sh
@@ -7,9 +7,9 @@ set -e
 # ComfyUI-KJNodes
 # @description: KJNodes is a collection of nodes for ComfyUI that are not part of the official ComfyUI repository.
 # @link: https://github.com/kijai/ComfyUI-KJNodes
-# if [ ! -d "$COMFY_BASE_DIRECTORY/custom_nodes/ComfyUI-KJNodes" ]; then
-#     echo "Installing ComfyUI-KJNodes..."
-#     git clone --branch main --depth 1 https://github.com/kijai/ComfyUI-KJNodes.git ${COMFY_BASE_DIRECTORY}/custom_nodes/ComfyUI-KJNodes
-# else
-#     echo "ComfyUI-KJNodes already installed"
-# fi
+if [ ! -d "$COMFY_BASE_DIRECTORY/custom_nodes/ComfyUI-KJNodes" ]; then
+    echo "Installing ComfyUI-KJNodes..."
+    git clone --branch main --depth 1 https://github.com/kijai/ComfyUI-KJNodes.git ${COMFY_BASE_DIRECTORY}/custom_nodes/ComfyUI-KJNodes
+else
+    echo "ComfyUI-KJNodes already installed"
+fi
