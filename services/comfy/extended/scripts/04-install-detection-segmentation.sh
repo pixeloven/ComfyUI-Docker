0ffc523ae8933e8f4d2292a713cb06771862762e
Brian Gebel <Brian Gebel>
2025-08-02 17:06:37 -0700

    fix

--- services/comfy/extended/scripts/04-install-detection-segmentation.sh
+++ services/comfy/extended/scripts/04-install-detection-segmentation.sh
@@ -12,7 +12,8 @@ log_info "Starting object detection and segmentation installation..."
 # ComfyUI-Impact-Pack
 # @description: Object detection, segmentation, and selective processing with advanced masking capabilities
 # @link: https://github.com/ltdrdata/ComfyUI-Impact-Pack
-install_custom_node "ComfyUI-Impact-Pack" "ComfyUI-Impact-Pack"
+# Using git installation due to CLI reliability issues
+install_custom_node_from_git "ComfyUI-Impact-Pack" "https://github.com/ltdrdata/ComfyUI-Impact-Pack.git"
 
 # ComfyUI-Impact-Subpack
 # @description: Additional components and utilities for ComfyUI-Impact-Pack
